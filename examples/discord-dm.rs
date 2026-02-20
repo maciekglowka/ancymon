9934a290215a1b1949d80c0d1e189ac851b610a9
maciek.glowka <maciek.glowka@gmail.com>
2026-02-20 21:39:28 +0100

    discord dm handler

---
+++ examples/discord-dm.rs
@@ -0,0 +1,19 @@
+use ancymon::{handlers::discord::DiscordDmBuilder, triggers::StartupTrigger, Bot, Config};
+use std::fs;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .init();
+
+    let config_str = fs::read_to_string("examples/discord-dm.toml").unwrap();
+    let config = Config::new(&config_str).unwrap();
+
+    Bot::default()
+        .with_handler_type("discord-dm", DiscordDmBuilder)
+        .with_source_type("startup", StartupTrigger::default())
+        .run(config)
+        .await
+        .unwrap();
+}
--- src/handlers/discord.rs
+++ src/handlers/discord.rs
@@ -1,8 +1,9 @@
 use async_trait::async_trait;
-use serenity::all::Http;
+use serde::Deserialize;
+use serenity::all::{CreateMessage, Http, PrivateChannel, UserId};
 
 use crate::{
-    errors::AncymonError,
+    errors::{AncymonError, BuildError, ConfigError, RuntimeError},
     handlers::{EventHandler, HandlerBuilder},
     shared::discord::get_http,
     values::Value,
@@ -11,19 +12,34 @@ use crate::{
 /// Sends DM messages to selected user.
 pub struct DiscordDmHandler {
     http: Http,
-    user_id: u64,
+    channel: PrivateChannel,
 }
 #[async_trait]
 impl EventHandler for DiscordDmHandler {
     async fn init(&mut self, config: &Value) -> Result<(), AncymonError> {
-        // self.user_id = config
-        //     .get("user-id")
-        //     .ok_or(ConfigError::MissingValue(format!("")))
+        let config: DiscordDmConfig = config.clone().try_into()?;
+        let user_id = config.user_id.parse().map_err(|e| {
+            ConfigError::InvalidValue(format!("Discord user id cannot be parsed: {e}"))
+        })?;
+        let user_id = UserId::new(user_id);
+        self.channel = user_id.create_dm_channel(&self.http).await.map_err(|e| {
+            BuildError::Handler(format!("Cannot create Discord private channel: {e}"))
+        })?;
         Ok(())
     }
-    async fn execute(&self, event: &Value, _arguments: &Value) -> Result<Value, AncymonError> {
-        tracing::debug!("{event:?}");
-        Ok(event.clone())
+    async fn execute(&self, event: &Value, arguments: &Value) -> Result<Value, AncymonError> {
+        let arguments: DiscordDmArguments = arguments.clone().try_into()?;
+        let mut content = arguments.message.to_string();
+        if arguments.include_event {
+            content += "\n";
+            content += &event.pretty();
+        }
+        let msg = self
+            .channel
+            .send_message(&self.http, CreateMessage::new().content(content))
+            .await
+            .map_err(|e| RuntimeError::Handler(format!("Discord DM sending failed: {e}")))?;
+        Ok(Value::String(msg.content))
     }
 }
 
@@ -32,7 +48,22 @@ impl HandlerBuilder for DiscordDmBuilder {
     fn build(&self) -> Result<Box<dyn EventHandler + Send + Sync>, AncymonError> {
         Ok(Box::new(DiscordDmHandler {
             http: get_http()?,
-            user_id: 0,
+            channel: PrivateChannel::default(),
         }))
     }
+}
+
+#[derive(Deserialize)]
+struct DiscordDmConfig {
+    #[serde(rename = "user-id")]
+    /// String is used as the id will most likely come from env variable.
+    user_id: String,
+}
+
+#[derive(Deserialize)]
+struct DiscordDmArguments {
+    message: String,
+    #[serde(default)]
+    #[serde(rename = "include-event")]
+    include_event: bool,
 }
