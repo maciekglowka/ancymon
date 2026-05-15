89d2a100769981a83d158e4dd09674ef001db017
maciek.glowka <maciek.glowka@gmail.com>
2026-05-15 09:38:21 +0200

    config parsing and var expansion

--- src/actions.rs
+++ src/actions.rs
@@ -2,24 +2,36 @@ use serde::Deserialize;
 
 use crate::values::Value;
 
-#[derive(Clone, Debug, Deserialize)]
+#[derive(Clone, Debug)]
 pub(crate) struct Action {
     pub(crate) handler: String,
     pub(crate) event: String,
     pub(crate) emit: String,
-    #[serde(default)]
     pub(crate) arguments: Value,
-
-    #[serde(default)]
-    #[serde(rename = "accepted-input")]
     pub(crate) accepted_input: AcceptedInput,
-
-    #[serde(default)]
-    #[serde(rename = "max-retries")]
     pub(crate) max_retries: usize,
-    #[serde(default)]
-    #[serde(rename = "retry-delay")]
     pub(crate) retry_delay: u64,
+}
+impl mlua::FromLua for Action {
+    fn from_lua(value: mlua::Value, _: &mlua::Lua) -> mlua::Result<Self> {
+        match value {
+            mlua::Value::Table(t) => Ok(Action {
+                handler: t.get("handler")?,
+                event: t.get("event")?,
+                emit: t.get("emit")?,
+                arguments: t.get("arguments").unwrap_or_default(),
+                accepted_input: t.get("accepted_input").unwrap_or_default(),
+                max_retries: t.get("max_retries").unwrap_or_default(),
+                retry_delay: t.get("retry_delay").unwrap_or_default(),
+            }),
+            v => Err(mlua::Error::FromLuaConversionError {
+                // FIXME
+                from: std::any::type_name_of_val(&v),
+                to: "Action".to_string(),
+                message: Some("Expected table".to_string()),
+            }),
+        }
+    }
 }
 
 #[derive(Clone, Copy, Debug, Default, Deserialize, PartialEq)]
@@ -29,4 +41,36 @@ pub enum AcceptedInput {
     Null,
     Ok,
     Err,
+}
+impl TryFrom<&str> for AcceptedInput {
+    type Error = String;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value {
+            "NotNull" => Ok(Self::NotNull),
+            "Null" => Ok(Self::Null),
+            "Ok" => Ok(Self::Ok),
+            "Err" => Ok(Self::Err),
+            v => Err(format!("Invalid str value for AcceptedInput variant {v}")),
+        }
+    }
+}
+impl mlua::FromLua for AcceptedInput {
+    fn from_lua(value: mlua::Value, _: &mlua::Lua) -> mlua::Result<Self> {
+        match value {
+            mlua::Value::String(s) => s.to_string_lossy().as_str().try_into().map_err(|e| {
+                mlua::Error::FromLuaConversionError {
+                    from: "Value::String",
+                    to: "AcceptedInput".to_string(),
+                    message: Some(e),
+                }
+            }),
+            v => Err(mlua::Error::FromLuaConversionError {
+                // FIXME
+                from: std::any::type_name_of_val(&v),
+                to: "AcceptedInput".to_string(),
+                message: Some("Expected string".to_string()),
+            }),
+        }
+    }
 }
--- src/config.rs
+++ src/config.rs
@@ -22,6 +22,8 @@ pub struct Config {
 }
 impl Config {
     pub fn new(s: &str) -> Result<Self, AncymonError> {
+        let s = Self::expand_env_variables(s)?;
+
         let lua = Lua::new();
         lua.load(s)
             .exec()
@@ -41,55 +43,37 @@ impl Config {
             .get::<Vec<Trigger>>("triggers")
             .map_err(|e| ConfigError::ParsingError(format!("Triggers array parse error: {e}")))?;
 
+        let actions = globals
+            .get::<Vec<Action>>("actions")
+            .map_err(|e| ConfigError::ParsingError(format!("Actions array parse error: {e}")))?;
+
         Ok(Self {
             sources,
             handlers,
-            actions: vec![],
+            actions,
             triggers,
         })
-
-        // let mut value: toml::Value =
-        //     toml::from_str(s).map_err(|e|
-        // ConfigError::ParsingError(format!("{e}")))?;
-
-        // Self::expand_env_variables(&mut value)?;
-
-        // value
-        //     .try_into::<Self>()
-        //     .map_err(|e| ConfigError::ParsingError(e.to_string()).into())
     }
-    // fn expand_env_variables(value: &mut toml::Value) -> Result<(), AncymonError>
-    // { match value {
-    //     toml::Value::String(s) => {
-    //         while let Some(capture) = ENV_REGEX.captures_iter(s).next() {
-    //             let outer =
-    // capture.get(0).ok_or(ConfigError::ParsingError(format!(
-    // "Invalid env variable placeholder at: {s}"             )))?;
-    //             let inner =
-    // capture.get(1).ok_or(ConfigError::ParsingError(format!(
-    // "Invalid env variable placeholder at: {s}"             )))?;
-    //             let var = inner.as_str();
-    //             let val = std::env::var(var).map_err(|_| {
-    //                 ConfigError::MissingValue(format!("Env variable not defined:
-    // {var}"))             })?;
-    //             let range = outer.range();
-    //             s.replace_range(range, &val);
-    //         }
-    //     }
-    //     toml::Value::Array(a) => {
-    //         for v in a.iter_mut() {
-    //             Self::expand_env_variables(v)?;
-    //         }
-    //     }
-    //     toml::Value::Table(t) => {
-    //         for (_, v) in t.iter_mut() {
-    //             Self::expand_env_variables(v)?;
-    //         }
-    //     }
-    //     _ => (),
-    // };
-    //     Ok(())
-    // }
+    fn expand_env_variables(s: &str) -> Result<String, AncymonError> {
+        let mut s = s.to_string();
+
+        while let Some(capture) = ENV_REGEX.captures_iter(&s).next() {
+            let outer = capture.get(0).ok_or(ConfigError::ParsingError(format!(
+                "Invalid env variable placeholder at: {s}"
+            )))?;
+            let inner = capture.get(1).ok_or(ConfigError::ParsingError(format!(
+                "Invalid env variable placeholder at: {s}"
+            )))?;
+            let var = inner.as_str();
+            let val = std::env::var(var).map_err(|_| {
+                ConfigError::MissingValue(format!("Env variable not defined: {var}"))
+            })?;
+            let range = outer.range();
+            s.replace_range(range, &val);
+        }
+
+        Ok(s)
+    }
 }
 
 #[cfg(test)]
@@ -128,6 +112,17 @@ mod tests {
                 }
             }
 
+            actions = {
+                {
+                    ["handler"]="discord-dm",
+                    ["event"]="cron-trigger",
+                    ["emit"]="discord-dm-event",
+                    ["arguments"]={
+                        ["message"]="Notification",
+                        
+                    }
+                }                
+            }
         "#;
 
         let config = Config::new(config_str).unwrap();
@@ -178,91 +173,22 @@ mod tests {
             config.triggers[1].arguments,
             Value::String("*/5 * * * * *".to_string())
         );
-    }
 
-    #[test]
-    fn expand_str() {
-        unsafe {
-            std::env::set_var("ANCYMON_TEST_FIRST", "1234");
-        }
-        unsafe {
-            std::env::set_var("ANCYMON_TEST_SECOND", "5678");
-        }
-        let mut val = toml::Value::String(
-            "my value is: ${ANCYMON_TEST_FIRST}:${ANCYMON_TEST_SECOND}".to_string(),
+        assert_eq!(1, config.actions.len());
+        assert_eq!(config.actions[0].handler, "discord-dm".to_string());
+        assert_eq!(config.actions[0].event, "cron-trigger".to_string());
+        assert_eq!(config.actions[0].emit, "discord-dm-event".to_string());
+        assert_eq!(
+            config.actions[0].arguments,
+            Value::Map(HashMap::from_iter(vec![(
+                "message".to_string(),
+                Value::String("Notification".to_string())
+            )]))
         );
-        // Config::expand_env_variables(&mut val).unwrap();
-        // assert_eq!("my value is: 1234:5678", val.as_str().unwrap());
     }
 
     #[test]
-    fn expand_str_twice() {
-        unsafe {
-            std::env::set_var("ANCYMON_TEST_ONLY", "987");
-        }
-        let mut val = toml::Value::String(
-            "my value is: ${ANCYMON_TEST_ONLY}:${ANCYMON_TEST_ONLY}:".to_string(),
-        );
-        // Config::expand_env_variables(&mut val).unwrap();
-        // assert_eq!("my value is: 987:987:", val.as_str().unwrap());
-    }
-
-    #[test]
-    fn expand_table() {
-        unsafe {
-            std::env::set_var("ANCYMON_TEST_MAP", "SECRET1234");
-        }
-        let mut val = toml::Value::Table(toml::Table::from_iter(vec![
-            (
-                "public".to_string(),
-                toml::Value::String("NOT_SECRET".to_string()),
-            ),
-            (
-                "private".to_string(),
-                toml::Value::String(":${ANCYMON_TEST_MAP}:".to_string()),
-            ),
-        ]));
-
-        // Config::expand_env_variables(&mut val).unwrap();
-        // assert_eq!(
-        //     ":SECRET1234:",
-        //     val.as_table()
-        //         .unwrap()
-        //         .get("private")
-        //         .unwrap()
-        //         .as_str()
-        //         .unwrap()
-        // );
-        // assert_eq!(
-        //     "NOT_SECRET",
-        //     val.as_table()
-        //         .unwrap()
-        //         .get("public")
-        //         .unwrap()
-        //         .as_str()
-        //         .unwrap()
-        // );
-    }
-
-    #[test]
-    fn expand_array() {
-        unsafe {
-            std::env::set_var("ANCYMON_TEST_ARR", "SECRET987");
-        }
-
-        let mut val = toml::Value::Array(vec![
-            toml::Value::String("NOT_SECRET".to_string()),
-            toml::Value::String(":${ANCYMON_TEST_ARR}:".to_string()),
-        ]);
-
-        // Config::expand_env_variables(&mut val).unwrap();
-        // assert_eq!("NOT_SECRET",
-        // val.as_array().unwrap()[0].as_str().unwrap()); assert_eq!(":
-        // SECRET987:", val.as_array().unwrap()[1].as_str().unwrap());
-    }
-
-    #[test]
-    fn expand_config() {
+    fn expand_variables() {
         unsafe {
             std::env::set_var("ANCYMON_TEST_SECRET", "$%^#");
         }
@@ -270,53 +196,64 @@ mod tests {
             std::env::set_var("ANCYMON_TEST_ID", "997");
         }
         let config_str = r#"
-          [sources.startup]  
-          type = "startup"
+            sources = {}
+            sources["startup"] = {["type"]="startup"}
 
-          [[triggers]]
-          source = "startup"
-          emit = "start"
+            triggers = {
+                {
+                    ["source"]="startup",
+                    ["emit"]="start"
+                },
+            }
 
-          [handlers.msg]
-          type = "mail"
-          arguments.secret = "${ANCYMON_TEST_SECRET}"
-          arguments.id = "${ANCYMON_TEST_ID}"
+            handlers = {}            
 
-          [[actions]]
-          handler = "msg"
-          event = "start"
-          emit = ""
+            handlers["msg"] = {
+                ["type"]="mail",
+                ["arguments"] = {
+                    ["secret"]="${ANCYMON_TEST_SECRET}",
+                    ["id"]="${ANCYMON_TEST_ID}"
+                }
+            }
+
+            actions = {
+                {
+                    ["handler"]="msg",
+                    ["event"]="start",
+                    ["emit"]="",
+                }                
+            }
         "#;
 
         let config = Config::new(config_str).unwrap();
 
-        // assert_eq!(
-        //     config.handlers["msg"]
-        //         .as_map()
-        //         .unwrap()
-        //         .get("arguments")
-        //         .unwrap()
-        //         .as_map()
-        //         .unwrap()
-        //         .get("secret")
-        //         .unwrap()
-        //         .as_str()
-        //         .unwrap(),
-        //     "$%^#"
-        // );
-        // assert_eq!(
-        //     config.handlers["msg"]
-        //         .as_map()
-        //         .unwrap()
-        //         .get("arguments")
-        //         .unwrap()
-        //         .as_map()
-        //         .unwrap()
-        //         .get("id")
-        //         .unwrap()
-        //         .as_str()
-        //         .unwrap(),
-        //     "997"
-        // );
+        assert_eq!(
+            config.handlers["msg"]
+                .as_map()
+                .unwrap()
+                .get("arguments")
+                .unwrap()
+                .as_map()
+                .unwrap()
+                .get("secret")
+                .unwrap()
+                .as_str()
+                .unwrap(),
+            "$%^#"
+        );
+        assert_eq!(
+            config.handlers["msg"]
+                .as_map()
+                .unwrap()
+                .get("arguments")
+                .unwrap()
+                .as_map()
+                .unwrap()
+                .get("id")
+                .unwrap()
+                .as_str()
+                .unwrap(),
+            "997"
+        );
     }
 }
